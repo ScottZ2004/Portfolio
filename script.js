c6336b7296cd52ece56adc88efd3bad9523bb665
Scott van den Broek <scottzico2004@gmail.com>
2022-09-05 16:52:17 +0200

    Modal van the story of is af, begonnen aan Lam

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Contact
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -10,3 +11,33 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+//Modal
+
+//TSO
+const open_TSO = document.getElementById('open_TSO');
+const modal_container_TSO = document.getElementById('modal_container_TSO');
+const close_TSO = document.getElementById('close_TSO');
+
+open_TSO.addEventListener('click', () =>{
+    modal_container_TSO.classList.add('show');
+})
+
+close_TSO.addEventListener('click', () =>{
+    modal_container_TSO.classList.remove('show');
+})
+
+
+//LAM
+const open_LAM = document.getElementById('open_LAM');
+const modal_container_LAM = document.getElementById('modal_container_LAM');
+const close_LAM = document.getElementById('close_LAM');
+
+open_LAM.addEventListener('click', () =>{
+    modal_container_LAM.classList.add('show');
+})
+
+close_LAM.addEventListener('click', () =>{
+    modal_container_LAM.classList.remove('show');
+})
+//HFL
