7b37646dd42de993211c9c3c03fa6dc80ccf74fe
Scott van den Broek <scottzico2004@gmail.com>
2022-09-13 12:46:04 +0200

    aan elk project een modal toegevoegd

--- script.js
+++ script.js
@@ -41,3 +41,50 @@ close_LAM.addEventListener('click', () =>{
     modal_container_LAM.classList.remove('show');
 })
 //HFL
+const open_HFL = document.getElementById('open_HFL');
+const modal_container_HFL = document.getElementById('modal_container_HFL');
+const close_HFL = document.getElementById('close_HFL');
+
+open_HFL.addEventListener('click', () =>{
+    modal_container_HFL.classList.add('show');
+})
+
+close_HFL.addEventListener('click', () =>{
+    modal_container_HFL.classList.remove('show');
+})
+//ES
+const open_ES = document.getElementById('open_ES');
+const modal_container_ES = document.getElementById('modal_container_ES');
+const close_ES = document.getElementById('close_ES');
+
+open_ES.addEventListener('click', () =>{
+    modal_container_ES.classList.add('show');
+})
+
+close_ES.addEventListener('click', () =>{
+    modal_container_ES.classList.remove('show');
+})
+//TC
+const open_TC = document.getElementById('open_TC');
+const modal_container_TC = document.getElementById('modal_container_TC');
+const close_TC = document.getElementById('close_TC');
+
+open_TC.addEventListener('click', () =>{
+    modal_container_TC.classList.add('show');
+})
+
+close_TC.addEventListener('click', () =>{
+    modal_container_TC.classList.remove('show');
+})
+//SM
+const open_SM = document.getElementById('open_SM');
+const modal_container_SM = document.getElementById('modal_container_SM');
+const close_SM = document.getElementById('close_SM');
+
+open_SM.addEventListener('click', () =>{
+    modal_container_SM.classList.add('show');
+})
+
+close_SM.addEventListener('click', () =>{
+    modal_container_SM.classList.remove('show');
+})
